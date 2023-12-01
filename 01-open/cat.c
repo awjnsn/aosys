e812e5574ae476550717ca5b0cb68cc1903cd7fd
Christian Dietrich <dietrich@ibr.cs.tu-bs.de>
2023-12-01 00:00:00 +0000

    Dec 1 -- The cat on the tip of the iceberg
    Article:  https://ibr.cs.tu-bs.de/advent/01-open/
    Workload: ~46 source-code lines

---
+++ 01-open/cat.c
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]) {
+    // For cat, we have to iterate over all command-line arguments of
+    // our process, where argv[0] is our program binary itself ("./cat").
+    for (int idx = 1; idx < argc; idx++) {
+        printf("argv[%d] = %s\n", idx, argv[idx]);
+    }
+
+    return 0;
+}
